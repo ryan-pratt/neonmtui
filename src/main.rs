f74401091d8c28c7fa5eba19115ab90e9f6adde7
Ryan Pratt <ryan.pratt@live.com>
2026-09-12 12:59:07 -0600

    list networks

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::{Context, Result};
+use nmrs::NetworkManager;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let nm = NetworkManager::new().await?;
+
+    let networks = nm
+        .list_networks(None)
+        .await
+        .context("failed to list networks")?;
+    for net in &networks {
+        println!("{} - Signal: {}%", net.ssid, net.strength.unwrap_or(0));
+    }
+
+    let ssid = nm
+        .current_ssid()
+        .await
+        .context("failed to get current SSID")?;
+    println!("Connected to: {}", ssid);
+
+    Ok(())
 }
